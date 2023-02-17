0d4227427cecd690565f969498b8ca4bf7b612ba
Lerato <leratodubej@gmail.com>
2023-02-17 05:18:30 -0800

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	long long int dd;
 	float ee;
 printf("Size of a char:%lu bytes(s)\n", (unsigned long)sizeof(aa));
-printf("Size of a int:%lu bytes(s)\n", (unsigned long)sizeof(bb));
+printf("Size of an int:%lu bytes(s)\n", (unsigned long)sizeof(bb));
 printf("Size of a long int:%lu bytes(s)\n", (unsigned long)sizeof(cc));
 printf("Size of a long long int:%lu bytes(s)\n", (unsigned long)sizeof(dd));
 printf("Size of a float:%lu bytes(s)\n", (unsigned long)sizeof(ee));
