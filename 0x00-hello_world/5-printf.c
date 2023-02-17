e2350cd09a1e6389fac84461a21e6bee539c88d5
Lerato <leratodubej@gmail.com>
2023-02-17 02:36:35 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - A program that prints using printf
- * return: 0(success)
+ * main - A program that prints using printf function
+ * Return: 0(success)
  */
 int main(void)
 { printf("with proper grammar, but the outcome is a piece of art,\n")
