8bfc599e59310ec576b4d08ff69a2b669a0cacd7
Lerato <leratodubej@gmail.com>
2023-02-17 17:08:07 -0800

    9errors

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 int m;
-for (m = 48; n < 58; m++)
+for (m = 48; m < 58; m++)
 {putchar (m);
 if (m != 57)
 {putchar (',');
-putchar ('');
+putchar (' ');
 }
 putchar('\n');
 return (0);
