3f6a9cc46367465d3c2894400ae42b57511cadc7
Lerato <leratodubej@gmail.com>
2023-02-22 09:17:18 -0800

    4alpha

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
- * main - check the code
- * Return: always 0
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -10,9 +12,9 @@ r = _isalpha('H');
 _putchar(r + '0');
 r = _isalpha('o');
 _putchar(r + '0');
-r = _isalpha(100);
+r = _isalpha(108);
 _putchar(r + '0');
-r = _isalpha(':');
+r = _isalpha(';');
 _putchar(r + '0');
 _putchar('\n');
 return (0);
