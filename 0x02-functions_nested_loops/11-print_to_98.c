79fea601a64391c4c3eff0a8a30f84f8c2ecd510
Lerato <leratodubej@gmail.com>
2023-02-22 13:15:44 -0800

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,10 +15,14 @@ for (; num <= 98; num++)
 if (num == 98)
 {
 printf("%d", num);
-printf("\n");
-break;
+}
+}
 }
 else
+{
+for (; num >= 98; num--)
+{
+if (num == 98)
 {
 printf("%d", num);
 printf("\n");
