33cf9d879b085c91eeeb83e508c18ec02f6579ea
Lerato <leratodubej@gmail.com>
2023-03-20 13:30:54 -0700

    halfway save changes

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * init_dog - initialize variabe dog
+ *
+ * Return : 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+dog.name = "Poppy"
+dog.age = 3.5
+dog.owner = "Bob"
+}
