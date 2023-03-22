4be20b96e6ad328d0c3a4b3cdf47f6e7b1ccddad
Lerato <leratodubej@gmail.com>
2023-03-22 11:33:42 -0700

    initdog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,7 +6,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-dog.name = "Poppy"
-dog.age = 3.5
-dog.owner = "Bob"
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
 }
