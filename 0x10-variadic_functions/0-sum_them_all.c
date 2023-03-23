419a965a33062e8d579c5c4e62c58f9f03487a30
Lerato <leratodubej@gmail.com>
2023-03-23 13:29:17 -0700

    no0errors

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,9 +12,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 va_list argu;
 va_start(argu, n);
-unsigned int sum = 0;
-for (unsigned int i = 0;i < n; i++)
+while (n != 0)
+{
+unsigned int i = 0;
+int sum = 0;
 sum = sum + va_arg(argu, int);
+i++;
 va_end(argu);
-return (sum);
+}
+return (0);
 }
