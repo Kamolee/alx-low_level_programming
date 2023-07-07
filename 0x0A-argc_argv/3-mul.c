d984fd20345528e7c55914879dd8624d6636e864
root <leratodubej@gmail.com>
2023-07-07 12:45:12 -0700

    errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,30 +9,31 @@
 int _atoi(char *s)
 {
 
-int i, d, m, leng, f, digit;       
+int i, d, m, leng, f, digit;
 
 i = 0;
-d = 0;            
+d = 0;
 m = 0;
 leng = 0;
 f = 0;
 digit = 0;
 while (s[leng] != '\0')
-len++
+leng++;
 while (i < leng && f == 0)
 {
 
-if (s[i] == '-') 
+if (s[i] == '-')
 ++d;
 if (s[i] >= '0' && s[i] <= '9')
 {
-digit = s[i] - '0';                
+digit = s[i] - '0';
+
 if (d % 2)
 digit = -digit;
 m = m * 10 + digit;
 f = 1;
 
-if (s[i + 1] < '0' || s[i + 1] > '9'
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
 
 f = 0;
@@ -48,8 +49,8 @@ return (m);
 * @argc: number of arguments
 * @argv: array of arguments
 *
-* Return: 0 (Success), 1 (Error)               
-*/
+* Return: 0 (Success), 1 (Error)
+ */
 int main(int argc, char *argv[])
 {
 int result, numb, numb1;
@@ -57,11 +58,10 @@ if (argc < 3 || argc > 3)
 {
 printf("Error\n");
 return (1);
-
 }
 numb = _atoi(argv[1]);
 numb1 = _atoi(argv[2]);
 result = numb * numb1;
-printf("%d\n", result);
+printf ("%d\n", result);
 return (0);
 }
