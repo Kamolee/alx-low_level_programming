2d53dd13802ff2b96ae8700ea776e88e0d2dd8af
root <leratodubej@gmail.com>
2023-07-27 07:09:06 -0700

    betty errors

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,7 +4,6 @@
 
 
 /**
-
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,24 +15,24 @@ list_t *add_node_end(list_t **head, const char *str)
 
 {
 
- list_t *new;
- list_t *temp = *head;
- unsigned int l = 0;
- while (str[l])
- l++;
+list_t *new;
+list_t *temp = *head;
+unsigned int l = 0;
+while (str[l])
+l++;
 
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
 
- new->str = strdup(str);
- new->len = l;
- new->next = NULL;
+new->str = strdup(str);
+new->len = l;
+new->next = NULL;
 
 
- if (*head == NULL)
- {
+if (*head == NULL)
+{
 *head = new;
 return (new);
 }
